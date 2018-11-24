0ae7b6f4ec408c39b14e1be2e428883344cf9b85
liuna123456 <356415624@qq.com>
2018-11-24 14:26:32 +0800

    config/dev.env.js

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,7 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+    NODE_ENV: '"development"',
+    // 刘娜的接口****使用的时候只需   precess.env.self_skim   =>   http://12.231.25.1:8080 
+    self_skim: '"http://12.231.25.1:8080"'
 })
